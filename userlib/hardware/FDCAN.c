5cbdad3228fb70077c2910c176319cfa70b6150f
Chosen <1779730593@qq.com>
2024-10-14 22:00:05 +0800

    Adaptability improved, allowing changable ID & length

--- userlib/hardware/FDCAN.c
+++ userlib/hardware/FDCAN.c
@@ -1,14 +1,13 @@
-#include "stm32g4xx_hal.h"
 #include "FDCAN.h"
 
 
-void FDCAN_SendData(FDCAN_HandleTypeDef* hfdcan, uint32_t ID, uint8_t TxData[])
+void FDCAN_SendData(FDCAN_HandleTypeDef* hfdcan, uint32_t ID, uint8_t TxData[], uint8_t length)
 {
 	FDCAN_TxHeaderTypeDef FDCAN_TxHeader;
-	FDCAN_TxHeader.Identifier = 0x200;
+	FDCAN_TxHeader.Identifier = ID;
 	FDCAN_TxHeader.IdType = FDCAN_STANDARD_ID;
 	FDCAN_TxHeader.TxFrameType = FDCAN_DATA_FRAME;
-	FDCAN_TxHeader.DataLength = 8;
+	FDCAN_TxHeader.DataLength = length;
 	FDCAN_TxHeader.ErrorStateIndicator = FDCAN_ESI_ACTIVE;
 	FDCAN_TxHeader.BitRateSwitch = FDCAN_BRS_OFF;
 	FDCAN_TxHeader.FDFormat = FDCAN_CLASSIC_CAN;
