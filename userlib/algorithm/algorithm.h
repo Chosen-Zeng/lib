2f155da8e10369f86b6dbc84e7f432a30d873922
Chosen <1779730593@qq.com>
2024-11-24 02:38:32 +0800

    collect common macro function and macro replacement

---
+++ userlib/algorithm/algorithm.h
@@ -0,0 +1,40 @@
+#ifndef __ALGORITHM_H
+#define __ALGORITHM_H
+
+#include "filtering.h"
+
+/** @defgroup macro replacement
+ * @{
+ */
+
+// limit X to +-Y
+#define ABS(X) ((X) >= 0 ? (X) : -(X))
+
+/** }@ */
+
+/** @defgroup macro function
+ * @{
+ */
+
+// limit X to 0 ~ Y
+#define LIMIT(X, Y)     \
+    if (Y >= 0)         \
+    {                   \
+        if (X < 0)      \
+            X = 0;      \
+        else if (X > Y) \
+            X = Y;      \
+    }                   \
+    else                \
+    {                   \
+        if (X > 0)      \
+            X = 0;      \
+        else if (X < Y) \
+            X = Y;      \
+    }
+#define ABS_LIMIT(X, Y) \
+    if (ABS(X) > Y)     \
+        X >= 0 ? (X = Y) : (X = -Y);
+
+/** }@ */
+#endif
