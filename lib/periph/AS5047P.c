c96219f9dc5eeaab8d21714987e0499ade022a49
Chosen Zeng <1779730593@qq.com>
2025-02-03 01:02:36 +0800

    AS5047P lib, using sw FIFO to comm. Can't realize non-volatile reg programming yet

--- lib/periph/AS5047P.c
+++ lib/periph/AS5047P.c
@@ -1,8 +1,28 @@
 #include "AS5047P.h"
+#include "TIM.h"
+#include <stdlib.h>
+
+#if defined AS5047P_NUM
+
+AS5047P_t AS5047P[AS5047P_NUM];
+unsigned short AS5047P_RxData[AS5047P_NUM];
+AS5047P_msg_node *AS5047P_node_recv[AS5047P_NUM], *AS5047P_node_head[AS5047P_NUM], // AS5047P_node_head[] is meaningful only at first recv
+    *AS5047P_node_send[AS5047P_NUM], *AS5047P_node_tail[AS5047P_NUM];
+
+#define AS5047P_TYPE_CMD_R 0
+#define AS5047P_TYPE_CMD_W 1
+#define AS5047P_TYPE_DATA 2
+
+static struct
+{
+    unsigned char ERRFL_msg : 1;    // ERRFL msg in FIFO
+    unsigned char PROGEN_check : 1; // PROGEN set
+    unsigned char PROG_msg : 1;     // PROG msg in FIFO
+} AS5047P_state_bit[AS5047P_NUM];
 
 unsigned char ParityCalc(unsigned short data)
 {
-    unsigned char cnt = 15, ret = 0;
+    unsigned char cnt = 16, ret = 0;
     while (cnt--)
     {
         ret ^= data >> cnt;
@@ -11,11 +31,220 @@ unsigned char ParityCalc(unsigned short data)
     return ret & 1;
 }
 
-unsigned char AS5047P_ReadData(SPI_TypeDef *SPI_handle, unsigned short AS5047P_reg)
+// @attention all msg should be added in one time, or exception may occur due to msg added previously being sent already
+void AS5047P_FIFO_Enqueue(unsigned char AS5047P_ID, unsigned short AS5047P_msg)
 {
-    return SPI_SendData(SPI_handle, ParityCalc(AS5047P_R | AS5047P_reg) << 15 | AS5047P_R | AS5047P_reg);
+    // not empty FIFO
+    if (AS5047P_node_tail[AS5047P_ID])
+    {
+        // create new node
+        AS5047P_node_tail[AS5047P_ID]->next = malloc(sizeof(AS5047P_msg_node));
+        AS5047P_node_tail[AS5047P_ID]->next->next = NULL;
+        AS5047P_node_tail[AS5047P_ID]->next->prev = AS5047P_node_tail[AS5047P_ID];
+        AS5047P_node_tail[AS5047P_ID] = AS5047P_node_tail[AS5047P_ID]->next;
+
+        // AS5047P_node_send[AS5047P_ID] point at new msg
+        if (!AS5047P_node_send[AS5047P_ID])
+            AS5047P_node_send[AS5047P_ID] = AS5047P_node_tail[AS5047P_ID];
+    }
+    // empty FIFO
+    else
+    {
+        // create new node
+        AS5047P_node_head[AS5047P_ID] = AS5047P_node_send[AS5047P_ID] = AS5047P_node_tail[AS5047P_ID] = malloc(sizeof(AS5047P_msg_node)); // recv node is null initially
+        AS5047P_node_tail[AS5047P_ID]->prev = AS5047P_node_tail[AS5047P_ID]->next = NULL;
+    }
+    AS5047P_node_tail[AS5047P_ID]->msg = AS5047P_msg;
+
+    // FIFO elmt > 1
+    if (AS5047P_node_tail[AS5047P_ID]->prev) // node is created above, ensuring the existence of AS5047P_node_tail[AS5047P_ID]
+        switch (AS5047P_node_tail[AS5047P_ID]->prev->type)
+        {
+        case AS5047P_TYPE_CMD_R:
+        case AS5047P_TYPE_DATA:
+        {
+            AS5047P_node_tail[AS5047P_ID]->type = AS5047P_msg & 0x4000 ? AS5047P_TYPE_CMD_R : AS5047P_TYPE_CMD_W;
+
+            break;
+        }
+        case AS5047P_TYPE_CMD_W:
+        {
+            AS5047P_node_tail[AS5047P_ID]->type = AS5047P_TYPE_DATA;
+
+            break;
+        }
+        }
+    // FIFO elmt == 1
+    else
+        AS5047P_node_tail[AS5047P_ID]->type = AS5047P_msg & 0x4000 ? AS5047P_TYPE_CMD_R : AS5047P_TYPE_CMD_W;
 }
 
-// void SPI5_IRQHandler(void)
-//{
-// }
+// @note node to dequeue is pointed at by AS5047P_node_recv[AS5047P_ID]
+void AS5047P_FIFO_Dequeue(unsigned char AS5047P_ID)
+{
+    // not empty FIFO
+    if (AS5047P_node_recv[AS5047P_ID])
+    {
+        // FIFO elmt > 1
+        if (AS5047P_node_recv[AS5047P_ID]->next)
+        {
+            AS5047P_node_recv[AS5047P_ID] = AS5047P_node_recv[AS5047P_ID]->next;
+            free(AS5047P_node_recv[AS5047P_ID]->prev);
+            AS5047P_node_recv[AS5047P_ID]->prev = NULL;
+        }
+        // FIFO elmt == 1
+        else
+        {
+            free(AS5047P_node_recv[AS5047P_ID]);
+            AS5047P_node_recv[AS5047P_ID] = AS5047P_node_send[AS5047P_ID] = AS5047P_node_tail[AS5047P_ID] = NULL;
+        }
+    }
+    // initial recv
+    else if (AS5047P_node_head[AS5047P_ID])
+    {
+        AS5047P_node_recv[AS5047P_ID] = AS5047P_node_head[AS5047P_ID];
+        AS5047P_node_head[AS5047P_ID] = NULL;
+    }
+}
+
+// @brief read specific reg
+inline void AS5047P_FIFO_R(unsigned char AS5047P_ID, unsigned short AS5047P_reg)
+{
+    AS5047P_FIFO_Enqueue(AS5047P_ID, AS5047P_R | AS5047P_reg);
+}
+
+// @brief write specific reg
+inline void AS5047P_FIFO_W(unsigned char AS5047P_ID, unsigned short AS5047P_reg, unsigned short data)
+{
+    AS5047P_FIFO_Enqueue(AS5047P_ID, AS5047P_W | AS5047P_reg);
+    AS5047P_FIFO_Enqueue(AS5047P_ID, data);
+}
+
+// @brief inquire pos by default, or send msg in FIFO if not empty
+// @exception mem exception occurs if interrupt not triggered; msg added after send function before IRQHandler cause sequence error
+unsigned char AS5047P_SendFIFO(SPI_TypeDef *SPI_handle, unsigned char AS5047P_ID)
+{
+    static timer_t AS5047P_time_PROG, AS5047P_time_ERRFL;
+
+    if (!TIMSW_TimeLimit(&AS5047P_time_PROG, 1) &&    // minimum check time: 1s
+        AS5047P_state_bit[AS5047P_ID].PROGEN_check && // PROGEN should be set
+        !AS5047P[AS5047P_ID].PROGEN &&                // PROGEN not set
+        !AS5047P_state_bit[AS5047P_ID].PROG_msg)      // PROG msg not in FIFO
+    {
+        AS5047P_FIFO_R(AS5047P_ID, AS5047P_PROG);
+
+        AS5047P_state_bit[AS5047P_ID].PROG_msg = 1;
+        AS5047P_time_PROG.curr = AS5047P_time_PROG.interval = AS5047P_time_PROG.prev = 0; // clear timer
+    }
+
+    if (!TIMSW_TimeLimit(&AS5047P_time_ERRFL, 1) && // minimum check time: 1s
+        AS5047P[AS5047P_ID].err &&                  // error bit
+        !AS5047P_state_bit[AS5047P_ID].ERRFL_msg)   // ERRFL msg not in FIFO
+    {
+        AS5047P_FIFO_R(AS5047P_ID, AS5047P_ERRFL);
+        AS5047P_FIFO_R(AS5047P_ID, AS5047P_DIAAGC);
+
+        AS5047P_state_bit[AS5047P_ID].ERRFL_msg = 1;
+        AS5047P_time_ERRFL.curr = AS5047P_time_ERRFL.interval = AS5047P_time_ERRFL.prev = 0; // clear timer
+    }
+
+    // specific msg
+    if (AS5047P_node_send[AS5047P_ID])
+    {
+        if (SPI_SendData(SPI_handle, ParityCalc(AS5047P_node_send[AS5047P_ID]->msg) << 15 | AS5047P_node_send[AS5047P_ID]->msg))
+            return 1;
+
+        AS5047P_node_send[AS5047P_ID] = AS5047P_node_send[AS5047P_ID]->next;
+    }
+    // common msg
+    else if (SPI_SendData(SPI_handle, ParityCalc(AS5047P_R | AS5047P_ANGLECOM) << 15 | AS5047P_R | AS5047P_ANGLECOM))
+        return 1;
+
+    return 0;
+}
+
+void AS5047P_MsgDecode(unsigned char AS5047P_ID)
+{
+    // FIFO mode
+    if (!ParityCalc(AS5047P_RxData[AS5047P_ID])) // verify data integrity
+    {
+        if (AS5047P_node_recv[AS5047P_ID] &&                           // specific msg
+            AS5047P_node_recv[AS5047P_ID]->type == AS5047P_TYPE_CMD_R) // msg to recv is read
+        {
+            if (AS5047P_node_recv[AS5047P_ID]->msg == (AS5047P_R | AS5047P_ERRFL))
+                AS5047P[AS5047P_ID].err = AS5047P_state_bit->ERRFL_msg = 0;
+            else
+                AS5047P[AS5047P_ID].err = (AS5047P_RxData[AS5047P_ID] & 0x4000) >> 14; // check error bit
+
+            switch (AS5047P_node_recv[AS5047P_ID]->msg & 0x3FFF) // match the reg
+            {
+            case AS5047P_DIAAGC:
+            {
+                AS5047P[AS5047P_ID].MAGL = (AS5047P_RxData[AS5047P_ID] & 0x800) >> 11;
+                AS5047P[AS5047P_ID].MAGH = (AS5047P_RxData[AS5047P_ID] & 0x400) >> 10;
+                AS5047P[AS5047P_ID].AGC = AS5047P_RxData[AS5047P_ID] & 0xFF;
+
+                break;
+            }
+            case AS5047P_PROG:
+            {
+                AS5047P_state_bit->PROG_msg = 0;
+                AS5047P[AS5047P_ID].PROGEN = AS5047P_RxData[AS5047P_ID];
+
+                break;
+            }
+            }
+        }
+        // common msg
+        else if (!AS5047P_node_recv[AS5047P_ID])
+        {
+            AS5047P[AS5047P_ID].err = (AS5047P_RxData[AS5047P_ID] & 0x4000) >> 14; // check error bit
+            AS5047P[AS5047P_ID].pos = 360.f * (AS5047P_RxData[AS5047P_ID] & 0x3FFF) / (1 << 14);
+        }
+    }
+
+    // update next recv msg info
+    AS5047P_FIFO_Dequeue(AS5047P_ID);
+}
+
+// @brief prepare for non-volatile reg program
+inline void AS5047P_ProgramInit(unsigned char AS5047P_ID)
+{
+    AS5047P_FIFO_W(AS5047P_ID, AS5047P_PROG, 0x9);
+
+    AS5047P_state_bit[AS5047P_ID].PROGEN_check = 1;
+}
+
+// @brief end of non-volatile reg program
+inline void AS5047P_ProgramDone(unsigned char AS5047P_ID)
+{
+    AS5047P_FIFO_W(AS5047P_ID, AS5047P_PROG, 0x44);
+
+    AS5047P[AS5047P_ID].PROGEN = AS5047P_state_bit[AS5047P_ID].PROGEN_check = 0;
+}
+
+void DMA1_Channel1_IRQHandler(void)
+{
+    DMA1->IFCR |= 0x2;
+
+    AS5047P_MsgDecode(0);
+
+    // disable SPI
+    while (SPI1->SR & 0x1880)
+        ;
+    SPI1->CR1 &= 0xFFBF;
+}
+
+// for better recv exp, refer to DMA_IRQHandler
+void SPI1_IRQHandler(void)
+{
+    AS5047P_RxData[0] = SPI1->DR;
+
+    AS5047P_MsgDecode(0);
+
+    // disable SPI
+    while (SPI1->SR & 0x1880)
+        ;
+    SPI1->CR1 &= 0xFFBF;
+}
+#endif
--- lib/periph/AS5047P.h
+++ lib/periph/AS5047P.h
@@ -4,11 +4,8 @@
 #include "user.h"
 #include "SPI.h"
 
-/** @defgroup AS5047P_reg
- *  @{
- */
+#if defined AS5047P_NUM
 
-/** @brief Volatile Registers */
 #define AS5047P_NOP 0x0000
 #define AS5047P_ERRFL 0x0001
 #define AS5047P_PROG 0x0003
@@ -17,99 +14,39 @@
 #define AS5047P_ANGLEUNC 0x3FFE
 #define AS5047P_ANGLECOM 0x3FFF
 
-/** @brief Non-Volatile Registers(OTP) */
 #define AS5047P_ZPOSM 0x0016
 #define AS5047P_ZPOSL 0x0017
 #define AS5047P_SETTINGS1 0x0018
 #define AS5047P_SETTINGS2 0x0019
 
-/** @brief SPI command complement */
 #define AS5047P_W 0x0000
 #define AS5047P_R 0x4000
-/** @} */
 
-typedef union
+typedef struct
 {
-    struct
-    {
-        uint8_t FRERR : 1;
-        uint8_t INVCOMM : 1;
-        uint8_t PARERR : 1;
-        uint8_t : 5;
-    } ERRFL;
-    struct
-    {
-        uint8_t PROGEN : 1;
-        uint8_t : 1;
-        uint8_t OTPREF : 1;
-        uint8_t PROGOTP : 1;
-        uint8_t : 2;
-        uint8_t PROGVER : 1;
-        uint8_t : 1;
-    } PROG;
-    struct
-    {
-        uint16_t AGC : 8;
-        uint16_t LF : 1;
-        uint16_t COF : 1;
-        uint16_t MAGH : 1;
-        uint16_t MAGL : 1;
-        uint16_t : 4;
-    } DIAAGC;
-    struct
-    {
-        uint16_t MAG : 14;
-        uint16_t : 2;
-
-    } MAG;
-    struct
-    {
-        uint16_t ANGLEUNC : 14;
-        uint16_t : 2;
-    } ANGLEUNC;
-    struct
-    {
-        uint16_t ANGLECOM : 14;
-        uint16_t : 2;
-    } ANGLECOM;
-} AS5047P_t_VLT;
-
-typedef union
-{
-    struct
-    {
-        uint8_t ZPOSM : 8;
-    } ZPOSM;
-    struct
-    {
-        uint8_t ZPOSL : 6;
-        uint8_t comp_l_error_en : 1;
-        uint8_t comp_h_error_en : 1;
-    } ZPOSL;
-    struct
-    {
-        uint8_t : 2;
-        uint8_t DIR : 1;
-        uint8_t UVW_ABI : 1;
-        uint8_t DAECDIS : 1;
-        uint8_t ABIBIN : 1;
-        uint8_t Dataselect : 1;
-        uint8_t PWMon : 1;
-    } SETTINGS1;
-    struct
-    {
-        uint8_t UVWPP : 3;
-        uint8_t HYS : 2;
-        uint8_t ABIRES : 3;
-    } SETTINGS2;
-} AS5047P_t_nVLT;
-
-typedef union
-{
-    AS5047P_t_VLT AS5047P_VLT;
-    AS5047P_t_nVLT AS5047P_nVLT;
+    float pos;
+    unsigned char err : 1, MAGL : 1, MAGH : 1, AGC, PROGEN : 1;
 } AS5047P_t;
+extern AS5047P_t AS5047P[AS5047P_NUM];
 
-unsigned char AS5047P_ReadData(SPI_TypeDef *SPI_handle, unsigned short AS5047P_reg);
+typedef struct
+{
+    unsigned short msg;
+    unsigned char type;
+} AS5047P_msg_t;
 
+typedef struct AS5047P_msg_node
+{
+    unsigned short msg;
+    unsigned char type;
+    struct AS5047P_msg_node *prev, *next;
+} AS5047P_msg_node;
+
+void AS5047P_FIFO_R(unsigned char AS5047P_ID, unsigned short AS5047P_reg);
+void AS5047P_FIFO_W(unsigned char AS5047P_ID, unsigned short AS5047P_reg, unsigned short data);
+unsigned char AS5047P_SendFIFO(SPI_TypeDef *SPI_handle, unsigned char AS5047P_ID);
+void AS5047P_ProgramInit(unsigned char AS5047P_ID);
+void AS5047P_ProgramDone(unsigned char AS5047P_ID);
+
+#endif
 #endif
